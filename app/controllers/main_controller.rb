1e2a5952d393b39d0f1834555f2ab7b3c06d7b62
cikai <cikai0923@gmail.com>
2016-10-24 23:30:54 +0800

    add md5 utils

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,14 @@
 class MainController < ApplicationController
   def index
   end
+
+  def md5utils
+  	require 'digest/md5'
+  	@md5str = params[:md5str]
+  	if @md5str.nil?
+  		@thirtyTwo = nil
+  		return
+  	end
+  	@thirtyTwo = Digest::MD5.hexdigest(@md5str.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'main/index'
+  get 'main/md5utils'
 
-  root 'main#index'
+  root 'main#md5utils'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
